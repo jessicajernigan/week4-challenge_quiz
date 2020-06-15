e5ec7c0f19211a1af87f51e4b0e6f95919330fde
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-06-14 19:09:03 -0500

    finished scoring, working on 'submit button' eventListener

--- assets/script.js
+++ assets/script.js
@@ -2,26 +2,55 @@ var startBtn = document.querySelector("#start-btn");
 var viewHighScoresBtn = document.querySelector("#highscores-btn");
 var index = 0;
 var score = 0;
-var timeLeft = 20;
+var timeLeft = 60;
 var totalQuestions = 2;
+var inputField = document.querySelector("#initials-field");
+var submitBtn = document.querySelector("#sign-up");
 
 // The array of questions for our quiz game.
 var questions = [
   {
-    q: "Which pet is the best?",
-    a1: "Baasha",
-    a2: "Cooper",
-    a3: "Fred",
-    a4: "Trick question; all of the above.",
+    q: "What is an alert()?",
+    a1: "A feature that alerts the developer when she's made a mistake.",
+    a2: "A function that prints 'alert' on the page.",
+    a3: "A custom variable that stores multiple boolean values.",
+    a4: "A function built into the browser that allows the developer to send messages to the user.",
     correct: "a4",
   },
   {
     q:
-      "What is the minimum amount of sleep required by 99.9999% of the human population?",
-    a1: "7+ hours",
-    a2: "6 hours",
-    a3: "5 hours",
-    a4: "4 hours",
+      "What does a plus sign (+) do?",
+    a1: "Contatenate strings",
+    a2: "Produce sums",
+    a3: "Concatenate strings or produce sums, depending on the context.",
+    a4: "Trick question! Plus signs are never used in JavaScript.",
+    correct: "a3",
+  },
+  {
+    q:
+      "What is the significance of semicolons (;) in JavaScript?",
+    a1: "They are a 'terminator,' alerting the browser that a particular line is complete and that any code after it is a new expression.",
+    a2: "They alert the browser to break out of the current function if a preceding value is false.",
+    a3: "They do not have a purpose; using them is entirely up to personal preference!",
+    a4: "They are used to create comments in a .js file.",
+    correct: "a1",
+  },
+  {
+    q:
+      "What insignia is used to verify whether two values are directly equal to one another?",
+    a1: "-=",
+    a2: "===",
+    a3: "/* */",
+    a4: "||",
+    correct: "a2",
+  },
+  {
+    q:
+      "Define null.",
+    a1: "The absence of a value.",
+    a2: "A slang term for a boring function. Try to spruce it up!",
+    a3: "A value less than zero.",
+    a4: "A negative value.",
     correct: "a1",
   },
 ];
@@ -64,7 +93,12 @@ function quizQuestions(index) {
   var btns = [answer1, answer2, answer3, answer4];
 
   var answerCheck = function (e) {
+    var correct = document.getElementById("right-wrong");
+    correct.textContent = " ";
     if (e.target.textContent == questions[index][questions[index].correct]) {
+      score++
+      correct.textContent = "Correct!"
+      console.log(score);
       if (!(index >= questions.length - 1)) {
         quizQuestions(index + 1)
       } else {
@@ -75,6 +109,7 @@ function quizQuestions(index) {
     else {
       // subtract ten and assign the new value to the timer variable
       timeLeft -= 10
+      correct.textContent = "Incorrect :("
       if (!(index >= questions.length - 1)) {
         quizQuestions(index + 1)
       } else {
@@ -95,11 +130,20 @@ function quizQuestions(index) {
 
 function showInputContainer() {
   document.getElementById("input-container").style.display = "inline-block";
+
+  if (score > 3) {
+    document.getElementById("your-score").textContent = "You got " + score + " out of " + questions.length + " questions correct. Nice work!";
+  } else {
+    document.getElementById("your-score").textContent = "You got " + score + " out of " + questions.length + " questions correct. Study up and try again soon!";
+  }
+
   document.getElementById("leaderboard").style.display = "none";
   document.getElementById("intro-section").style.display = "none";
   document.getElementById("quiz-content").style.display = "none";
   document.getElementById("timer-div").style.display = "none";
 };
+
+
 
 function showLeaderboard() {
   document.getElementById("leaderboard").style.display = "inline-block";
@@ -109,12 +153,21 @@ function showLeaderboard() {
   document.getElementById("timer-div").style.display = "none";
 };
 
-// 1. Check for incorrect answer, then subtract time from 'timer'
-// 2. Stop quiz, go to form input (to get initials)
-// 3. Submission populates high scores (localStorage)
 
-startBtn.addEventListener("click", quizTimer); // Removes introductory text and starts the timer.
 viewHighScoresBtn.addEventListener("click", showLeaderboard);
+startBtn.addEventListener("click", quizTimer); // Removes introductory text and starts the timer.
+submitBtn.addEventListener("click", function (event) {
+  event.preventDefault()
+  var initials = document.querySelector("#initials-field").value;
+  
+  if (initials === "") {
+    displayMessage("error", "Please enter your initials.");
+  } else {
+    localStorage.setItem("initials", initials);
+    showLeaderboard();
+  }
+
+});
 
 // User Story
 // AS A coding boot camp student
